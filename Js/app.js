11d44a8ae0d44e1bef1fe30b1b378f6bda9d1193
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-02-02 20:05:06 +0330

    add user class

--- Js/app.js
+++ Js/app.js
@@ -2,3 +2,16 @@
 
 const canvas = document.getElementById('canvas')
 let ctx = canvas.getContext('2d')
+
+
+// User Class //
+class User {
+    constructor () {
+        
+    }
+    
+    userGamepad () {
+        ctx.fillRect(125, canvas.height - 10, 100, 10)
+        
+    }   
+}
