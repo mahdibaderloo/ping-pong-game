bc555be40e0971d9ff7434f7928475289a1e1f7f
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-04-08 10:02:25 +0330

    system moveRight method

--- Js/app.js
+++ Js/app.js
@@ -1,40 +1,43 @@
 // Dom Elements //
 
-const canvas = document.getElementById('canvas')
-let ctx = canvas.getContext('2d')
-
+const canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
 
 // User Class //
 class User {
-    constructor () {
-        
-    }
-    
-    userGamepad () {
-        ctx.fillRect(125, canvas.height - 10, 100, 10)
-        
-    }   
-}
+  constructor() {}
 
+  userGamepad() {
+    ctx.fillRect(125, canvas.height - 10, 100, 10);
+  }
+}
 
 // System Class //
 
 class System {
-    constructor () {
+  constructor() {
+    this.x = 0;
+  }
 
-    }
+  systemGamepad() {
+    ctx.fillRect(this.x, 0, 100, 10);
+  }
 
-    systemGamepad () {
-        ctx.fillRect(125, 0, 100, 10)
+  moveRight() {
+    for (let i = 0; i < canvas.width - 100; i++) {
+      ctx.fillRect(this.x, 0, 100, 0);
     }
+    this.x += 5;
+    ctx.fillRect(this.x, 0, 100, 10);
+  }
 }
 
 // Window //
 
-window.addEventListener('load', () => {
-    let user = new User ()
-    let system = new System ()
+window.addEventListener("load", () => {
+  let user = new User();
+  let system = new System();
 
-    user.userGamepad()
-    system.systemGamepad()
-})
+  user.userGamepad();
+  system.systemGamepad();
+});
